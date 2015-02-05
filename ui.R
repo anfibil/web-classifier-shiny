b2460cafe0b07c89e68c893a68b66f2fe88bcd39
Everaldo Aguiar <anfibil@gmail.com>
2015-02-04 23:35:16 -0500

    Got rid of annoying blank row

--- ui.R
+++ ui.R
@@ -1,11 +1,11 @@
-shinyUI(fluidPage(
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "http://bootswatch.com/united/bootstrap.css")),
-  navbarPage('Web-based Classification Tool',
+shinyUI(
+  navbarPage('Web-based Classification Tool', header = "",
+     #tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "http://bootswatch.com/united/bootstrap.css")),
      source("external/inputTab.R",local=T)$value,
      source("external/exploreTab.R",local=T)$value,
      source("external/classifyTab.R",local=T)$value
   )
-))  
+)  
 
 # There is an insisting blank row that keeps showing up below the navigation bar
 # Waiting for help here: http://goo.gl/QN0cug
