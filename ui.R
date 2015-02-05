933dd679cd0139bffc482c04ba46c584d2fcc0de
Everaldo Aguiar <anfibil@gmail.com>
2015-02-05 18:24:19 -0500

    Fixed window title

--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 shinyUI(
-  navbarPage(title=div(img(src="http://i.imgur.com/exTOHnM.png"), "  |   Web-based Classification Tool"), header = "", theme = shinytheme("spacelab"),
+  navbarPage(title=div(img(src="http://i.imgur.com/exTOHnM.png"), "  |   Web-based Classification Tool"), 
+             header = "", theme = shinytheme("spacelab"), windowTitle = "Web-based Classification Tool",
              source("external/inputTab.R",local=T)$value,
              source("external/exploreTab.R",local=T)$value,
              source("external/classifyTab.R",local=T)$value
